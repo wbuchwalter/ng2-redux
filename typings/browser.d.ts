c7b08640eaa3e31c31b2db1f2555c7622f97f385
Michael J. Bennett <michael.bennett@rangle.io>
2016-03-08 00:08:07 -0500

    Purge typings

--- typings/browser.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="browser/ambient/invariant/index.d.ts" />
-/// <reference path="browser/ambient/lodash/index.d.ts" />
-/// <reference path="browser/ambient/redux/index.d.ts" />
--- typings/main.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="main/ambient/invariant/index.d.ts" />
-/// <reference path="main/ambient/lodash/index.d.ts" />
-/// <reference path="main/ambient/redux/index.d.ts" />
