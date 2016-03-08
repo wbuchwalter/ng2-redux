1c24d8b2671107e531162e4eaf870983c3c99156
Michael J. Bennett <michael.bennett@rangle.io>
2016-03-07 22:35:54 -0500

    Upgraded tsd to typings

---
+++ typings/browser.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="browser/ambient/invariant/index.d.ts" />
+/// <reference path="browser/ambient/lodash/index.d.ts" />
+/// <reference path="browser/ambient/redux/index.d.ts" />
---
+++ typings/main.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="main/ambient/invariant/index.d.ts" />
+/// <reference path="main/ambient/lodash/index.d.ts" />
+/// <reference path="main/ambient/redux/index.d.ts" />
