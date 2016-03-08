6f4ed5ac32ce75e1aefba9e5e96952d6f8b832d6
Michael J. Bennett <michael.bennett@rangle.io>
2016-03-08 10:29:59 -0500

    Removes top level tmp directory

--- tmp/tsreq/src/components/utils/wrapActionCreators.js
+++
@@ -1,6 +0,0 @@
-var redux_1 = require('redux');
-function wrapActionCreators(actionCreators) {
-    return function (dispatch) { return redux_1.bindActionCreators(actionCreators, dispatch); };
-}
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = wrapActionCreators;
