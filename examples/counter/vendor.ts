47707cbc24bfbd058e2a0216c81e9dcb9380f930
Evan Schultz <evan@rangle.io>
2016-04-27 12:52:39 -0400

    remove extra import of browser

--- examples/counter/vendor.ts
+++ examples/counter/vendor.ts
@@ -1,6 +1,5 @@
 import 'es6-shim';
 import 'angular2/bundles/angular2-polyfills'
-import 'angular2/platform/browser';
 import 'angular2/platform/browser'
 import 'angular2/platform/common_dom'
 import 'angular2/core'
