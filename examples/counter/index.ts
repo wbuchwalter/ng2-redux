b221b512f7e7aaa0b31e64e514165473f4dacfe3
Seth Davenport <SethDavenport@users.noreply.github.com>
2016-04-28 12:42:58 -0400

    Chore/example project cleanup (#52)
    chore(example): use npm package instead of source.

--- examples/counter/index.ts
+++ examples/counter/index.ts
@@ -1,8 +1,7 @@
 import {bootstrap} from 'angular2/platform/browser';
 import {App} from './containers/App';
 import configureStore from './store/configureStore';
-import {provider} from '../../src';
-import {NgRedux} from '../../src';
+import {provider} from 'ng2-redux';
 
 const devTools = require('./devTools');
 const store = configureStore();
