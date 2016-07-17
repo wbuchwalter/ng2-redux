25c84c2e67ac2a3e832aaeb07574e87d30317471
Chris Bond <chris.bond@rangle.io>
2016-07-17 16:25:49 -0400

    Do not depend upon immutablejs, only use it if the project uses it

--- src/utils/get-in.ts
+++ src/utils/get-in.ts
@@ -1,18 +1,21 @@
-import * as immutable from 'immutable';
+let immutable;
+try {
+  immutable = require('immutable');
+} catch (e) {}
 
 /*
  * Gets a deeply-nested property value from an object, given a 'path'
  * of property names or array indices.
  */
 export function getIn(
-  v: Object | immutable.Iterable<string, any>,
+  v: Object,
   pathElems: (string | number)[]): any {
     if (!v) {
       return v;
     }
 
-    if (immutable.Iterable.isIterable(v)) {
-      return (<immutable.Iterable<string, any>>v).getIn(pathElems);
+    if (immutable && immutable.Iterable.isIterable(v)) {
+      return (<any>v).getIn(pathElems);
     }
 
     const [ firstElem, ...restElems] = pathElems;
