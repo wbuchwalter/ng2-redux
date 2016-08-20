03baa0bc1e7b8ea9f949eb39c6040d3c81dd90cc
Seth Davenport <seth@rangle.io>
2016-08-20 00:12:52 -0400

    fix unit tests, update build so they can live with their modules

--- src/components/ng-redux.ts
+++ src/components/ng-redux.ts
@@ -32,7 +32,7 @@ const checkSelector = (s) => VALID_SELECTORS.indexOf(typeof s, 0) >= 0 ||
     Array.isArray(s);
 
 export type PropertySelector = string | number | symbol;
-export type PathSelector =  (string | number)[];
+export type PathSelector = (string | number)[];
 export type FunctionSelector<RootState, S> = ((s: RootState) => S);
 export type Comparator = (x: any, y: any) => boolean;
 
