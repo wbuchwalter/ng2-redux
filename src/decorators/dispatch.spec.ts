996645acd7804e2ecd9c33eb01a103aada34b42c
Evan Schultz <evan@rangle.io>
2017-05-11 14:01:17 -0400

    chore(cleanup) Cleanup dispatch decorator
    * DRY up the unit tests a bit
    * Modify tsconfig setup so .spec files don't show errors in editor

--- src/decorators/dispatch.spec.ts
+++ src/decorators/dispatch.spec.ts
@@ -3,112 +3,106 @@ import { NgZone } from '@angular/core';
 import { NgRedux } from '../components/ng-redux';
 import { dispatch } from './dispatch';
 
+
 class MockNgZone {
   run = fn => fn()
 }
 
-fdescribe('@dispatch', () => {
+describe('@dispatch', () => {
   let ngRedux;
   const mockNgZone = new MockNgZone() as NgZone;
-  let targetObj;
   let defaultState;
   let rootReducer;
 
   beforeEach(() => {
     defaultState = {
-      foo: 'bar',
-      baz: -1
+      value: 'init-value',
+      instanceProperty: 'init-instanceProperty'
     };
     rootReducer = (state = defaultState, action) => {
-      const newState = Object.assign({}, state, { baz: action.payload });
-      return newState;
+      switch (action.type) {
+        case 'TEST':
+          const { value, instanceProperty } = action.payload;
+          return Object.assign({}, state, { value, instanceProperty });
+        default:
+          return state;
+      }
+
     };
-    targetObj = {};
     ngRedux = new NgRedux(mockNgZone);
     ngRedux.configureStore(rootReducer, defaultState);
   });
 
   it('should call dispatch with the result of the function', () => {
-    class TestClass {
-      instanceProperty = 'test'
-      @dispatch()
-      myMethod(value) {
-        return {
-          type: 'TEST',
-          payload: {
-            value,
-            instanceProperty: this.instanceProperty
-          }
-        }
-      }
-    }
+
     spyOn(NgRedux.instance, 'dispatch');
     const instance = new TestClass();
-    const result = instance.myMethod('test payload');
-    expect(result.type).toBe('TEST');
-    expect(NgRedux.instance.dispatch).toHaveBeenCalledWith({
-      type: 'TEST', payload: {
-        value: 'test payload',
+    const result = instance.classMethod('class method');
+    const expectedArgs = {
+      type: 'TEST',
+      payload: {
+        value: 'class method',
         instanceProperty: 'test'
       }
-    })
+    }
+    expect(result.type).toBe('TEST');
+    expect(result.payload.value).toBe('class method');
+    expect(result.payload.instanceProperty).toBe('test');
+    expect(NgRedux.instance.dispatch).toHaveBeenCalledWith(expectedArgs)
   });
 
   it('should work with property initalizers', () => {
-    class TestClass {
-      instanceProperty = 'test'
-      @dispatch()
-      boundProperty = (value) => {
-        return {
-          type: 'TEST',
-          payload: {
-            value,
-            instanceProperty: this.instanceProperty
-          }
-        }
-      }
-    }
+
     spyOn(NgRedux.instance, 'dispatch');
     const instance = new TestClass();
-    const result = instance.boundProperty('test payload');
-    expect(result.type).toBe('TEST');
-    expect(NgRedux.instance.dispatch).toHaveBeenCalledWith({
-      type: 'TEST', payload: {
-        value: 'test payload',
+    const result = instance.boundProperty('bound property');
+    const expectedArgs = {
+      type: 'TEST',
+      payload: {
+        value: 'bound property',
         instanceProperty: 'test'
       }
-    })
+    }
+    expect(result.type).toBe('TEST');
+    expect(result.payload.value).toBe('bound property');
+    expect(result.payload.instanceProperty).toBe('test');
+    expect(NgRedux.instance.dispatch).toHaveBeenCalledWith(expectedArgs)
   })
 
   it('work with properties bound to function defined outside of the class', () => {
-    function test(value) {
-      return {
-        type: 'TEST',
-        payload: {
-          value,
-          instanceProperty: this.instanceProperty
-        }
-      }
-    }
-    class TestClass {
-      instanceProperty = 'test'
-      @dispatch()
-      boundProperty = test;
-    }
     spyOn(NgRedux.instance, 'dispatch');
     const instance = new TestClass();
-    const result = instance.boundProperty('test payload');
-    expect(result.type).toBe('TEST');
-    expect(NgRedux.instance.dispatch).toHaveBeenCalledWith({
-      type: 'TEST', payload: {
-        value: 'test payload',
+    const result = instance.externalFunction('external function');
+    const expectedArgs = {
+      type: 'TEST',
+      payload: {
+        value: 'external function',
         instanceProperty: 'test'
       }
-    })
+    }
+    expect(result.type).toBe('TEST');
+    expect(result.payload.value).toBe('external function');
+    expect(result.payload.instanceProperty).toBe('test');
+    expect(NgRedux.instance.dispatch).toHaveBeenCalledWith(expectedArgs);
   })
 
-  it('work with properties bound to fat arrow defined outside of the class', () => {
-    const test = (value) => {
+
+
+  function externalFunction(value) {
+    return {
+      type: 'TEST',
+      payload: {
+        value,
+        instanceProperty: this.instanceProperty
+      }
+    }
+  }
+  class TestClass {
+    instanceProperty = 'test'
+    @dispatch()
+    externalFunction = externalFunction
+    @dispatch()
+    classMethod(value) {
       return {
         type: 'TEST',
         payload: {
@@ -117,20 +111,16 @@ fdescribe('@dispatch', () => {
         }
       }
     }
-    class TestClass {
-      instanceProperty = 'test'
-      @dispatch()
-      boundProperty = test;
-    }
-    spyOn(NgRedux.instance, 'dispatch');
-    const instance = new TestClass();
-    const result = instance.boundProperty('test payload');
-    expect(result.type).toBe('TEST');
-    expect(NgRedux.instance.dispatch).toHaveBeenCalledWith({
-      type: 'TEST', payload: {
-        value: 'test payload',
-        instanceProperty: undefined
+
+    @dispatch()
+    boundProperty = (value) => {
+      return {
+        type: 'TEST',
+        payload: {
+          value,
+          instanceProperty: this.instanceProperty
+        }
       }
-    })
-  })
+    }
+  }
 });
--- src/decorators/dispatch.ts
+++ src/decorators/dispatch.ts
@@ -2,24 +2,25 @@ import {
   NgRedux,
 } from '../components/ng-redux';
 
-
-export function dispatch(options?: any) {
-  return function dispatch(target, key, descriptor?) {
-
+export function dispatch(): void | any {
+  return function dispatchDecorator(target: object, key: string | symbol | number, descriptor?: PropertyDescriptor) {
     let originalMethod: Function;
+
+    descriptor = descriptor || Object.getOwnPropertyDescriptor(target, key);
     const wrapped = function (...args) {
+
       const result = originalMethod.apply(this, args);
       NgRedux.instance.dispatch(result);
       return result;
     }
+
     if (descriptor === undefined) {
       const dispatchDescriptor: PropertyDescriptor = {
         get: () => wrapped,
         set: (setMethod) => originalMethod = setMethod,
       }
       Object.defineProperty(target, key, dispatchDescriptor)
-
-      // return descriptor;
+      return;
     } else {
       originalMethod = descriptor.value;
       descriptor.value = wrapped;
