ee07bf2bf6ea8f7696e71f76db835917ea66ddb7
Seth Davenport <seth>
2017-05-29 21:49:25 -0400

    clean up unintentionally-commited build output

--- lib/src/components/fractal-reducer-map.d.ts
+++
@@ -1,8 +0,0 @@
-import { Reducer } from 'redux';
-import { PathSelector } from './selectors';
-/** @hidden */
-export declare function enableFractalReducers(rootReducer: Reducer<any>): Reducer<any>;
-/** @hidden */
-export declare function registerFractalReducer(basePath: PathSelector, localReducer: Reducer<any>): void;
-/** @hidden */
-export declare function replaceLocalReducer(basePath: PathSelector, nextLocalReducer: Reducer<any>): void;
